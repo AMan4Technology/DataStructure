5ef59015bf26f47489b06925c2e354b308cef687
AMan <35390271+aman4technology@users.noreply.github.com>
2019-03-29 09:27:17 +0800

    update bucketSort

--- useful/sort/Bucket.go
+++ useful/sort/Bucket.go
@@ -1,58 +1,58 @@
 package sort
 
 import (
-    . "DataStructure/useful/maxmin"
+	"DataStructure/useful/maxmin"
 )
 
 func Bucket(data []int) {
-    length := len(data)
-    if length < 2 {
-        return
-    }
-    var (
-        max, min = OfInt(data[0], data[1:]...)
-        i        int
-    )
-    if max-min <= 1<<10 {
-        buckets := make([]int, max-min+1)
-        for _, value := range data {
-            buckets[value-min]++
-        }
-        for value, nums := range buckets {
-            for ; nums != 0; nums-- {
-                data[i] = value + min
-                i++
-            }
-        }
-        return
-    }
-    var (
-        count   = MinOfTwoInt(1<<10, length)
-        size    = (max-min+1)/count + 1
-        buckets = make([]bucket, count)
-    )
-    for _, value := range data {
-        buckets[value/size] = append(buckets[value/size], value)
-    }
-    for _, bucket := range buckets {
-        Quick(bucket)
-        for _, value := range bucket {
-            data[i] = value
-            i++
-        }
-    }
+	length := len(data)
+	if length < 2 {
+		return
+	}
+	var (
+		max, min = maxmin.OfInt(data[0], data[1:]...)
+		i        int
+	)
+	if max-min <= 1<<10 {
+		buckets := make([]int, max-min+1)
+		for _, value := range data {
+			buckets[value-min]++
+		}
+		for value, nums := range buckets {
+			for ; nums != 0; nums-- {
+				data[i] = value + min
+				i++
+			}
+		}
+		return
+	}
+	var (
+		count   = maxmin.MinOfTwoInt(1<<10, length)
+		size    = (max-min+1)/count + 1
+		buckets = make([]bucket, count)
+	)
+	for _, value := range data {
+		buckets[(value-min)/size] = append(buckets[(value-min)/size], value)
+	}
+	for _, bucket := range buckets {
+		Quick(bucket)
+		for _, value := range bucket {
+			data[i] = value
+			i++
+		}
+	}
 }
 
 type bucket []int
 
 func (b bucket) Len() int {
-    return len(b)
+	return len(b)
 }
 
 func (b bucket) Less(i, j int) bool {
-    return b[i] < b[j]
+	return b[i] < b[j]
 }
 
 func (b bucket) Swap(i, j int) {
-    b[i], b[j] = b[j], b[i]
+	b[i], b[j] = b[j], b[i]
 }
