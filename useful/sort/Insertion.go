fdcb09d421ff957b23ab99b061691c555d28210b
AMan <35390271+aman4technology@users.noreply.github.com>
2019-03-29 10:35:28 +0800

    update Insertion support Sortable

--- useful/sort/Insertion.go
+++ useful/sort/Insertion.go
@@ -1,15 +1,12 @@
 package sort
 
-func Insertion(data []int) {
-    for length, i := len(data), 1; i < length; i++ {
-        curr := data[i]
-        j := i - 1
-        for ; j >= 0; j-- {
-            if data[j] <= curr {
-                break
-            }
-            data[j+1] = data[j]
-        }
-        data[j+1] = curr
-    }
+func Insertion(data Sortable) {
+	for length, i := data.Len(), 1; i < length; i++ {
+		for j := i; j > 0; j-- {
+			if !data.Less(j, j-1) {
+				break
+			}
+			data.Swap(j, j-1)
+		}
+	}
 }
